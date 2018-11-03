76f2653c0e5c6fcae842ce8d6faa665640d6f5b0
Konstantin Tsybizov <itandem@yandex.ru>
2018-11-03 12:43:05 +0400

    xhr commit

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -4,7 +4,10 @@ const request = new XMLHttpRequest();
 request.addEventListener('load', onLoad);
 request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', false);
 request.send();
-if (request.status === 200) {
-  const response = JSON.parse(request.responseText);
-  setData(response);
+
+function onLoad() {
+  if (request.status === 200) {
+    const response = JSON.parse(request.responseText);
+    setData(response);
+  }
 }
